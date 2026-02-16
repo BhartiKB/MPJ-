d532b07f66e41e115f3315729f6dfa7df4335825
BhartiKB <166482013+BhartiKB@users.noreply.github.com>
2026-02-16 23:28:52 +0530

    Update LAB02.java

--- LAB02.java
+++ LAB02.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 // ================= BASE CLASS =================
-class Employee {
-    protected String name;
+abstract class Employee {
+    private String name;
     protected double salary;
 
     // Constructor
@@ -12,18 +12,20 @@ class Employee {
         System.out.println("Employee constructor called");
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void displaySalary() {
         System.out.println("Employee Name: " + name);
         System.out.println("Salary: " + salary);
     }
 
-    @Override
-    protected void finalize() throws Throwable {
-        System.out.println("Employee object destroyed");
-    }
+    // Abstract method
+    public abstract void calculateSalary();
 }
 
-// ============ SINGLE INHERITANCE =============
+// ============ SINGLE + HIERARCHICAL ============
 class FullTimeEmployee extends Employee {
 
     FullTimeEmployee(String name, double salary) {
@@ -31,24 +33,20 @@ class FullTimeEmployee extends Employee {
         System.out.println("FullTimeEmployee constructor called");
     }
 
+    @Override
     public void calculateSalary() {
         System.out.println("\n--- Full Time Employee ---");
         System.out.println("Before Hike:");
         displaySalary();
 
-        salary = salary + (salary * 0.50);
+        salary += salary * 0.50;
 
         System.out.println("After 50% Hike:");
         displaySalary();
     }
-
-    @Override
-    protected void finalize() throws Throwable {
-        System.out.println("FullTimeEmployee object destroyed");
-    }
 }
 
-// ========== HIERARCHICAL INHERITANCE ==========
+// ========== HIERARCHICAL ==========
 class InternEmployee extends Employee {
 
     InternEmployee(String name, double salary) {
@@ -56,24 +54,20 @@ class InternEmployee extends Employee {
         System.out.println("InternEmployee constructor called");
     }
 
+    @Override
     public void calculateSalary() {
         System.out.println("\n--- Intern Employee ---");
         System.out.println("Before Hike:");
         displaySalary();
 
-        salary = salary + (salary * 0.25);
+        salary += salary * 0.25;
 
         System.out.println("After 25% Hike:");
         displaySalary();
     }
-
-    @Override
-    protected void finalize() throws Throwable {
-        System.out.println("InternEmployee object destroyed");
-    }
 }
 
-// ========== MULTILEVEL INHERITANCE ==========
+// ========== MULTILEVEL ==========
 class TraineeIntern extends InternEmployee {
 
     TraineeIntern(String name, double salary) {
@@ -85,64 +79,40 @@ class TraineeIntern extends InternEmployee {
         System.out.println("\n--- Trainee Intern ---");
         displaySalary();
     }
-
-    @Override
-    protected void finalize() throws Throwable {
-        System.out.println("TraineeIntern object destroyed");
-    }
 }
 
 // ================= MAIN CLASS =================
-class Mai {
+public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        // Input for Full Time Employee
         System.out.print("Enter Full Time Employee Name: ");
         String fName = sc.nextLine();
-        System.out.print("Enter Full Time Employee Salary: ");
+        System.out.print("Enter Salary: ");
         double fSalary = sc.nextDouble();
         sc.nextLine();
 
-        FullTimeEmployee fte = new FullTimeEmployee(fName, fSalary);
+        Employee fte = new FullTimeEmployee(fName, fSalary);
         fte.calculateSalary();
 
-        // Input for Intern
         System.out.print("\nEnter Intern Name: ");
         String iName = sc.nextLine();
-        System.out.print("Enter Intern Salary: ");
+        System.out.print("Enter Salary: ");
         double iSalary = sc.nextDouble();
         sc.nextLine();
 
-        InternEmployee intern = new InternEmployee(iName, iSalary);
+        Employee intern = new InternEmployee(iName, iSalary);
         intern.calculateSalary();
 
-        // Input for Trainee Intern
-        System.out.print("\nEnter Trainee Intern Name: ");
+        System.out.print("\nEnter Trainee Name: ");
         String tName = sc.nextLine();
-        System.out.print("Enter Trainee Intern Salary: ");
+        System.out.print("Enter Salary: ");
         double tSalary = sc.nextDouble();
 
         TraineeIntern trainee = new TraineeIntern(tName, tSalary);
         trainee.traineeInfo();
 
-        // Making objects eligible for GC
-        fte = null;
-        intern = null;
-        trainee = null;
-
-        System.gc();
-
         sc.close();
     }
 }
-
-
-
-/*
-- what is inheritance with proper example
-- what operator overloading
-- does operator overloading works in java?
-- 5 problems where inheritance is used
- */
